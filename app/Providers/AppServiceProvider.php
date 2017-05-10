d42deaf48c730b7a42a44ca757d44b2e841dfe39
mors84 <tomasz.morski@gmail.com>
2017-05-10 15:26:35 +0200

    AppServiceProvider has been updated.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,14 @@
 
 namespace App\Providers;
 
+use App\Comment;
+use App\CommentReply;
+use App\Post;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +20,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $countOfDisapprovedComments = Schema::hasTable('comments') ? Comment::where('is_active', 0)->count() : null;
+        $countOfDisapprovedReplies = Schema::hasTable('comment_replies') ? CommentReply::where('is_active', 0)->count() : null;
+        $countOfAllDisapprovedPosts = Schema::hasTable('posts') ? Post::where('is_active', 0)->count() : null;
+        $countOfAllDisapprovedUsers = Schema::hasTable('users') ? User::where('is_active', 0)->count() : null;
+
+        $countOfAllDisapprovedComments = $countOfDisapprovedComments + $countOfDisapprovedReplies;
+        $countOfAllDisapprovedAlerts = $countOfAllDisapprovedComments + $countOfAllDisapprovedPosts + $countOfAllDisapprovedUsers;
+
+        view()->share('countOfAllDisapprovedComments', $countOfAllDisapprovedComments);
+        view()->share('countOfAllDisapprovedPosts', $countOfAllDisapprovedPosts);
+        view()->share('countOfAllDisapprovedUsers', $countOfAllDisapprovedUsers);
+        view()->share('countOfAllDisapprovedAlerts', $countOfAllDisapprovedAlerts);
     }
 
     /**
@@ -23,6 +41,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // Set Carbon language
+        Carbon::setLocale(config('app.locale'));
     }
 }
