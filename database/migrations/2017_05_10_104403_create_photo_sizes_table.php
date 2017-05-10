9d3221d0650d3ce4751c23bff4d8286f191670ec
mors84 <tomasz.morski@gmail.com>
2017-05-10 13:57:44 +0200

    Models with migrations have been created.

---
+++ database/migrations/2017_05_10_104403_create_photo_sizes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotoSizesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photo_sizes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photo_sizes');
+    }
+}
