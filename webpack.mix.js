07c9964e9b1d9f12ef20acbcfdf06e1a9745ef2a
mors84 <tomasz.morski@gmail.com>
2017-05-11 12:05:15 +0200

    Webpack has been updated.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,40 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix
+    // Admin files
+    .copyDirectory('resources/assets/fonts/', 'public/fonts/')
+    .copyDirectory('resources/assets/images/', 'public/images/')
+    .copy('resources/assets/vendors/admin/css/chosen/', 'public/css/admin/chosen/')
+    .copy('resources/assets/sass/admin/patterns/', 'public/css/admin/patterns/')
+    .sass('resources/assets/sass/admin/app.scss', 'public/css/admin/app.css')
+    .combine([
+        'resources/assets/vendors/admin/css/bootstrap.css',
+        'resources/assets/vendors/admin/css/font-awesome.css',
+        'resources/assets/vendors/admin/css/animate.css',
+        'resources/assets/vendors/admin/css/bootstrap-chosen.css',
+        'resources/assets/vendors/admin/css/summernote.css',
+        'resources/assets/vendors/admin/css/summernote-bs3.css',
+        'resources/assets/vendors/admin/css/toastr.min.css',
+    ], 'public/css/admin/vendors.css')
+    .combine([
+        'resources/assets/js/admin/main.js',
+        'resources/assets/js/admin/ajax.js',
+        'resources/assets/js/admin/passwordMeter.js',
+        'resources/assets/js/admin/summernote.js',
+    ], 'public/js/admin/app.js')
+    .combine([
+        'resources/assets/vendors/admin/js/jquery-3.1.1.min.js',
+        'resources/assets/vendors/admin/js/bootstrap.min.js',
+        'resources/assets/vendors/admin/js/jquery.metisMenu.js',
+        'resources/assets/vendors/admin/js/jquery.slimscroll.min.js',
+        'resources/assets/vendors/admin/js/inspinia.js',
+        'resources/assets/vendors/admin/js/pace.min.js',
+        'resources/assets/vendors/admin/js/chosen.jquery.js',
+        'resources/assets/vendors/admin/js/summernote.min.js',
+        'resources/assets/vendors/admin/js/toastr.min.js',
+        'resources/assets/vendors/admin/js/pwstrength-bootstrap.min.js',
+        'resources/assets/vendors/admin/js/zxcvbn.js',
+    ], 'public/js/admin/vendors.js')
+
+    .disableNotifications();
