40635d56e0d8c465f9abca85b1d7b034659239a5
mors84 <tomasz.morski@gmail.com>
2017-05-11 09:25:58 +0200

    Routes have been created.

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,52 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+
+/**
+* Auth::routes() without Register
+*
+*
+*/
+Route::post('login', 'Auth\LoginController@login');
+Route::get('login',  'Auth\LoginController@showLoginForm')->name('login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+
+
+/**
+* Only for auth
+*
+*
+*/
+Route::group(['middleware' => 'auth'], function() {
+
+    Route::get('/admin', function(){
+        return view('admin.pages.home');
+    })->name('admin');
+
+});
+
+
+/**
+* Only for admins
+*
+*
+*/
+Route::group(['middleware' => 'admin'], function() {
+
+    Route::resource('/admin/categories', 'CategoryController', ['except' => 'show']);
+    Route::resource('/admin/media', 'MediaController', ['only' => 'index']);
+    Route::resource('/admin/media/photos', 'PhotoController', ['except' => 'show']);
+    Route::resource('/admin/posts', 'PostController');
+    Route::resource('/admin/tags', 'TagController', ['except' => 'show']);
+    Route::resource('/admin/users', 'UserController', ['except' => 'show']);
+    Route::post('/admin/tags/ajax', 'TagController@ajaxStore')->name('tags.ajaxStore');
+    Route::put('/admin/comments/active/{comment}', 'CommentController@ajaxChangeActive')->name('comments.ajaxChangeActive');
+    Route::put('/admin/replies/active/{reply}', 'CommentReplyController@ajaxChangeActive')->name('replies.ajaxChangeActive');
+    Route::put('/admin/posts/active/{post}', 'PostController@ajaxChangeActive')->name('posts.ajaxChangeActive');
+    Route::put('/admin/users/active/{user}', 'UserController@ajaxChangeActive')->name('users.ajaxChangeActive');
+
+});
